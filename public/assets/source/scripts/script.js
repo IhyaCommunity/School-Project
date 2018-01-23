9e7d05adfa97eef4f659b3a10fa28b81e02636ff
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2018-01-24 04:13:17 +0500

    public folder added to project and package.json edited

---
+++ public/assets/source/scripts/script.js
@@ -0,0 +1,10 @@
+var animal = /** @class */ (function () {
+    function animal(name, color) {
+        name = this.name;
+        color = this.color;
+    }
+    return animal;
+}());
+var Bird = new animal();
+
+//# sourceMappingURL=script.js.map
---
+++ public/assets/source/typescript/script.ts
@@ -0,0 +1,12 @@
+class animal {
+    name: string;
+    color: string;
+
+    constructor(name?: string, color?: string) {
+        name = this.name;
+        color = this.color;
+    }
+}
+
+var Bird = new animal();
+
